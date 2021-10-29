7790dd9922b09728b06aafaf0b27a856b684e2d6
josoriov <juanferov97@gmail.com>
2021-10-29 11:40:27 -0500

    Changing the project structure and removing unnecesary folders 🗂

--- exercises-applied/module02/src/test/scala/koans/Module02.scala
+++ exercises-applied/module02/src/test/scala/koans/Module02.scala
@@ -111,7 +111,7 @@ class Module02 extends KoanSuite with Matchers with SeveredStackTraces {
 
     // replace the following with the correct code to convert tuple t
     // to a list of strings
-    val l = Nil
+    val l = t.productIterator.toList.map(_.toString)
 
     l should be (List("0", "u", "8", "1", "too"))
   }
@@ -120,7 +120,7 @@ class Module02 extends KoanSuite with Matchers with SeveredStackTraces {
     var getSet = Set("Ready", "Steady")
 
     // Add a line below to satisfy the test
-
+    getSet = getSet ++ Set("Go!")
     getSet should be (Set("Ready", "Steady", "Go!"))
 
     // What happens if you make the var a val above? Why?
@@ -130,7 +130,7 @@ class Module02 extends KoanSuite with Matchers with SeveredStackTraces {
     var getSet = scala.collection.mutable.Set("Ready", "Steady")
 
     // Add a line below to satisfy the test
-
+    getSet = getSet ++ Set("Go!")
     getSet should be (Set("Ready", "Steady", "Go!"))
 
     // what happens if you make the var a val above? Why? Is this a good idea?
@@ -143,15 +143,15 @@ class Module02 extends KoanSuite with Matchers with SeveredStackTraces {
     mutMap += (2 -> "Dos")
     mutMap += (3 -> "Tres")
 
-    mutMap(2) should be (__)
+    mutMap(2) should be ("Dos")
 
     mutMap += (2 -> "Two")
 
-    mutMap(2) should be (__)
+    mutMap(2) should be ("Two")
 
     // What happens if you uncomment the line below? Why?
     // mutMap += (2 -> 2)
-    mutMap(2) should be (__)
+    mutMap(2) should be ("Two")
   }
 
   test ("Mutable map in a val") {
@@ -161,18 +161,18 @@ class Module02 extends KoanSuite with Matchers with SeveredStackTraces {
     mutMap += (2 -> "Dos")
     mutMap += (3 -> "Tres")
 
-    mutMap(2) should be (__)
+    mutMap(2) should be ("Dos")
 
     mutMap(2) = "Two"
 
-    mutMap(2) should be (__)
+    mutMap(2) should be ("Two")
 
     mutMap += (2 -> "Deux")
 
-    mutMap(2) should be (__)
+    mutMap(2) should be ("Deux")
 
     // What happens if you uncomment the line below? Why?
     // mutMap += (2 -> 2)
-    mutMap(2) should be (__)
+    mutMap(2) should be ("Deux")
   }
 }
